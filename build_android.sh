cbd6a43b26bbc864e2a5e7d079d67ceca3072cc0
Aniruddh <Kukadiya.ani@gmail.com>
2023-04-21 12:31:15 +0530

    updated script to post install android

--- build_android.sh
+++ build_android.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-IMAGE_NAME="react-native-pjsip-builder/android"
-CONTAINER_NAME="react-native-pjsip-builder-${RANDOM}"
+IMAGE_NAME="pjsip-release/android"
+CONTAINER_NAME="pjsip-release-${RANDOM}"
 
 rm -rf ./dist/android;
 mkdir -p ./dist/;
 
-docker build -t react-native-pjsip-builder/android ./android/;
+docker build -t pjsip-release/android ./android/;
 docker run --name ${CONTAINER_NAME} ${IMAGE_NAME} bin/true
 
 docker cp ${CONTAINER_NAME}:/dist/android ./dist/android
--- build_ios.sh
+++ build_ios.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-IMAGE_NAME="react-native-pjsip-builder/ios"
-CONTAINER_NAME="react-native-pjsip-builder-${RANDOM}"
+IMAGE_NAME="pjsip-release/ios"
+CONTAINER_NAME="pjsip-release-${RANDOM}"
 
 rm -rf ./dist/ios;
 mkdir -p ./dist/;
 
-docker build -t react-native-pjsip-builder/ios ./ios/;
+docker build -t pjsip-release/ios ./ios/;
 docker run --name ${CONTAINER_NAME} ${IMAGE_NAME} bin/true
 
 docker cp ${CONTAINER_NAME}:/dist/ios ./dist/ios
